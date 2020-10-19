cb9c6f4e7d0dd9caa6627cfc243ff33cfcf0110e
FOUED HAMOUDA <foha2001@yahoo.fr>
2020-10-19 15:48:04 +0100

    graph working now

--- R/cotation.R
+++ R/cotation.R
@@ -1,25 +1,16 @@
 ######## programme pour suivre l'évolution des cours de la Bourse
 #de TUNIS##########program 1V1##
-#' @title Tunisian firm cotation
-#'
+#' @title
 #' Get prices from Tunis Stock Exchange
 #' @param x the ticker symbol of firm
 #' @return Historical Prices Graph
-#' @import xts
 #' @export
 cotation=function(x){
-usethis::use_data(sample_data, overwrite = TRUE)
-sample_data$val=gsub("\\s+","",sample_data$val)
-adj=subset(sample_data, val=="x")
-date <- as.Date(as.character(adj$seance), "%Y-%m-%d")
-value=as.numeric(adj$p)
-library(zoo)
-library(xts)
-graf=xts(value,date)
-plot(graf, main = "Graph of your query ")
+#usethis::use_data(sample_data, internal = TRUE)
+#adj=subset(sample_data, lib=="x")
+ggplot2::ggplot(subset(sample_data, lib==x),ggplot2::aes(date,prix)) +
+        ggplot2::geom_line(color="blue", linetype = 19)
 }
-
-
 
 
 
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,10 +2,11 @@
 library(readxl)
 setwd("C:/Users/foha2/OneDrive/VAR model TUNINDEX/programmingart2")
 histo_cotation_2019 <- read_excel("histo_cotation_20199.xlsx")
-sample_data=data.frame(seance=histo_cotation_2019$SEANCE,val=histo_cotation_2019$LIB_VAL, p=histo_cotation_2019$CLOTURE)
-str_replace(" ", "", sample_data$val)
-
+sample_data=data.frame(seance=histo_cotation_20199$SEANCE,val=histo_cotation_20199$LIB_VAL, p=histo_cotation_20199$CLOTURE)
+sample_data$val=gsub("\\s+","",sample_data$val)
+date=sample_data$seance <- as.Date(as.character(sample_data$seance), "%Y-%m-%d")
+prix=sample_data$p=as.numeric(sample_data$p)
+lib=sample_data$val
+sample_data=data.frame(date,lib,prix)
 setwd("C:/Users/foha2/OneDrive/DOCUMENTS/tunisianfirms")
-
-
-usethis::use_data(sample_data, overwrite = T, compress = "xz")
+usethis::use_data(sample_data, overwrite = T)
