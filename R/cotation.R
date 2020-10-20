4af9ec19c22cbe2ef9a86e879f0c6899e68c863a
FOUED HAMOUDA <foha2001@yahoo.fr>
2020-10-20 11:45:46 +0100

    add new file

--- R/cotation.R
+++ R/cotation.R
@@ -1,13 +1,13 @@
 ######## programme pour suivre l'évolution des cours de la Bourse
 #de TUNIS##########program 1V1##
 #' @title
-#' Get prices from Tunis Stock Exchange
-#' @param x the ticker symbol of firm
+#' Show prices from Tunis Stock Exchange
+#' @param "x" the ticker symbol of firm
 #' @return Historical Prices Graph
+#' @examples
+#' cotation("STB")
 #' @export
 cotation=function(x){
-#usethis::use_data(sample_data, internal = TRUE)
-#adj=subset(sample_data, lib=="x")
 ggplot2::ggplot(subset(sample_data, lib==x),ggplot2::aes(date,prix)) +
         ggplot2::geom_line(color="blue", linetype = 19)
 }
---
+++ R/tunisianfirms.R
@@ -0,0 +1,13 @@
+# 'Tunisian firms package
+#'
+#' Contain function for creating graph for
+#' firms listed on TUNINDEX
+#'
+#' @seealso \code{\link{cotation}}
+#'
+#' @docType package
+#'
+#' @author Foued Hamouda \email{foha2001@gmail.com}
+#'
+#'@name tunisianfirms
+NULL
